d1e16bb9e7c4652c27d1b1b385a774223f46a6e5
yizhe <liaoyizhe75@gmail.com>
2023-07-20 16:19:27 +0800

    pracitces intersection、union、difference

---
+++ 0720/string compare.py
@@ -0,0 +1,7 @@
+# 2023/07/20
+A_list = {1, 3, 5, 7, 9, 11}
+B_list = {0, 5, 10}
+print(f'聯集：{A_list | B_list}')
+print(f'交集：{A_list & B_list}')
+print(f'A-B差集{A_list - B_list}')
+print(f'B-A差集{B_list - A_list}')
