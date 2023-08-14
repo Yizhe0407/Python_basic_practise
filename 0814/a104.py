713d185f73dc5329ea9bcb91493d153ec0392eef
yizhe <liaoyizhe75@gmail.com>
2023-08-14 20:22:45 +0800

    fix file

--- 0814/a104.py
+++ 0814/a104.py
@@ -28,3 +28,27 @@ try:
         print(' '.join(map(str, figures)))
 except EOFError:
     pass
+
+# 快速排序法
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+try:
+    while True:
+        how_many_number = int(input())
+        figures = list(map(int, input().split()))
+
+        sorted_figures = quick_sort(figures)
+
+        print(' '.join(map(str, sorted_figures)))
+except EOFError:
+    pass
