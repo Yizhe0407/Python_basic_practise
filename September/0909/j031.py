4e3657ac0392a0382fea79b14aaeac2822d8d057
yizhe <liaoyizhe75@gmail.com>
2023-09-09 17:51:54 +0800

    create new file

--- September/0909/j031.py
+++ September/0909/j031.py
@@ -1,3 +1,12 @@
-a, b, c, d, l = map(int, input().split())
-for x in range(l + 1):
-    f = (a * x) ** 2 + (b * x) + c
+while True:
+    a, b, c, d, l = map(int, input().split())
+    if a == b == c == d == l == 0:
+        break
+    if a >= -1000 and a <= 1000 and b >= -1000 and b <= 1000 and c >= -1000 and c <= 1000:
+        if d > 1 and d < 1000000 and l >= 0 and l < 1000:
+            divisible = 0
+            for x in range(0, l + 1):
+                f = a * (x ** 2) + (b * x) + c
+                if f % d == 0:
+                    divisible += 1
+            print(divisible)
